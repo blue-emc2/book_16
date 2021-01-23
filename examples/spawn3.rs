c238ceaaaee32f41471fce39a4f982bd94f1bbf7
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2021-01-23 20:43:36 +0900

    複数のメッセージを送信するようにする

--- examples/spawn3.rs
+++ examples/spawn3.rs
@@ -2,17 +2,33 @@
 
 use std::sync::mpsc;
 use std::thread;
+use std::time::Duration;
 
 fn main() {
   // tx: 転送
   // rx: 受信
   let (tx, rx) = mpsc::channel();
 
+  println!("spawn begin");
+
   thread::spawn(move || {
-    let val = String::from("hi");
-    tx.send(val).unwrap();
+    let vals = vec![
+      String::from("hi"),
+      String::from("from"),
+      String::from("the"),
+      String::from("thread"),
+    ];
+
+    for val in vals {
+      println!("send begin");
+      tx.send(val).unwrap();
+      thread::sleep(Duration::from_secs(1));
+    }
   });
 
-  let received = rx.recv().unwrap();
-  println!("Got: {}", received);
+  println!("received begin");
+  for received in rx {
+    println!("Got: {}", received);
+  }
+  println!("received end");
 }
